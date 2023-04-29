767474d6db784bc21cd76f26e7ea3aafe1b48518
Patrik <patrik@qmusicplayer.se>
2023-04-29 14:39:48 +0200

    refactored code creating copyFile and copyDir so it works recursively. Also wrapped git functions in async since no sync version of them acutally exists.

--- bin/create.js
+++ bin/create.js
@@ -3,6 +3,7 @@
 const yargs = require('yargs');
 const fs = require('fs');
 const path = require('path');
+const cp = require('child_process');
 const simpleGit = require('simple-git');
 
 const argv = yargs
@@ -36,39 +37,77 @@ if (!fs.existsSync(argv.path)) {
 }
 
 if (!/^[\w-]+$/.test(argv.name)) {
+	console.error(argv.name);
 	console.error('The module name can only contain letters, numbers, hyphens, and underscores.');
 	process.exit(1);
 }
 
-const boilerplatePath = path.join(argv.path, argv.name);
+const boilerplatePath = path.join(__dirname, '../boilerplate');
+const outputPath = path.join(path.resolve(argv.path), argv.name);
 
-console.log('Copying boilerplate files...');
-fs.mkdirSync(boilerplatePath);
-fs.readdirSync(path.join(__dirname, '../boilerplate')).forEach((file) => {
-	//Read the template file
-	const oldPath = path.join(__dirname, '../boilerplate', file);
-	let contents = fs.readFileSync(oldPath, 'utf8');
+if (fs.existsSync(outputPath))
+	throw new Error("That path already exists: " + outputPath)
+
+console.log("Creating", outputPath);
+fs.mkdirSync(outputPath);
+
+
+console.log('Starting to copy boilerplate files there...');
+copyDir(boilerplatePath, outputPath)
+
+
+function copyFile(src, trg) {
+
+	//Read contents
+	let contents = fs.readFileSync(src, 'utf8');
 
 	//Replace placeholders
 	contents = contents.replace(/xx-package-name-xx/g, argv.name);
 	contents = contents.replace(/xx-git-username-xx/g, argv.github);
   
 	//Write it to the new location
-	const newPath = path.join(boilerplatePath, file);
-	fs.writeFileSync(newPath, contents);
-});
+	console.log(src, '-->', trg);
+	fs.writeFileSync(trg, contents);
+}
+
+function copyDir(sourcePath, targetPath) {
+	const files = fs.readdirSync(sourcePath);
+	for (const file of files) {
+		const src = path.join(sourcePath, file);
+		const trg = path.join(targetPath, file);
+		if (fs.statSync(src).isDirectory()) {
+			if (file == 'node_modules' || file == 'dist')
+				continue;
+			fs.mkdirSync(trg);
+			copyDir(src, trg);
+		} else {
+			copyFile(src, trg)
+		}
+	}
+}
 
 console.log('Installing npm dependencies...');
-const execSync = require('child_process').execSync;
-execSync(`cd ${boilerplatePath} && npm install`);
+cp.execSync(`cd ${outputPath} && npm install --omit=dev`);
 
-console.log('Initializing git repository and configuring remote...');
-const git = simpleGit(boilerplatePath);
-git.initSync();
-git.addRemoteSync('origin', `https://github.com/${argv.github}/${argv.name}.git`);
+(async function asyncGit() {
+	try {	
+		console.log('Initializing git repository and configuring remote...');
+		const git = simpleGit(outputPath);
+		await git.init();
+		await git.addRemote('origin', `https://github.com/${argv.github}/${argv.name}.git`);
+		await git.branch(['-u', 'origin/master']); //set the upstream branch
 
-console.log('Creating first commit...');
-git.addSync('*');
-git.commitSync('boilerplate created');
+		console.log('Creating first commit...');
+		await git.add('*');
+		await git.commit('boilerplate created');
 
-console.log('Boilerplate created successfully.');
+		console.log("NOTE: You now have a local git repo with a single commit. Before you can push it to github");
+		console.log("      however you have to create the remote repo, either by logging into github or by running:");
+		console.log(`            gh repo create --name ${argv.name}`);
+
+		console.log('Boilerplate created successfully.');
+	} catch (e) {
+		console.error('FATAL:', e);
+		process.exit(1);
+	}
+})()
